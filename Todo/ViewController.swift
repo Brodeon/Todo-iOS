661b579583dd574287cb496c30d94a4a523bc375
Przemek <przemek@LinuxPluss-Mac.local>
2019-02-01 17:43:01 +0100

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Przemek on 2/1/19.
+//  Copyright © 2019 Przemek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
